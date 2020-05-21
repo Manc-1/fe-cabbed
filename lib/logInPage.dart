d3a300f8d9a013a8c264012efdf8c1e3cc2e002f
Luke Flannery <lukeflannery@Lukes-MBP-2.broadband>
2020-05-21 20:52:03 +0100

    refactored file structure separating pages

---
+++ lib/logInPage.dart
@@ -0,0 +1,297 @@
+// import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'signUp.dart';
+import 'main.dart';
+// import 'package:http/http.dart' as http;
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool _rememberMe = false;
+
+// Future<String> postData() async{
+//   var response = await http.post("https://be-cabbed.herokuapp.com/api/users/login",
+//   headers
+//   );
+// }
+
+  Widget _buildEmail() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: Colors.grey.withOpacity(0.5),
+            border: Border(
+              bottom: BorderSide(
+                color: Hexcolor('#FFB600'),
+                width: 3.0,
+              ),
+            ),
+          ),
+          height: 60,
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(color: Colors.white, fontFamily: "Poppins"),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.white,
+              ),
+              hintText: "Enter your email",
+              hintStyle: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPassword() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: Colors.grey.withOpacity(0.5),
+            border: Border(
+              bottom: BorderSide(
+                color: Hexcolor('#FFB600'),
+                width: 3.0,
+              ),
+            ),
+          ),
+          height: 60,
+          child: TextField(
+            obscureText: true,
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.white,
+              ),
+              hintText: "Enter your password",
+              hintStyle: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildForgotPassword() {
+    return Container(
+      alignment: Alignment.centerRight,
+      child: FlatButton(
+        onPressed: () => print("forgot password button pressed"),
+        padding: EdgeInsets.only(right: 0.0),
+        child: Text(
+          "Forgot password?",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRememberMe() {
+    return Container(
+      height: 20.0,
+      child: Row(
+        children: <Widget>[
+          Theme(
+            data: ThemeData(unselectedWidgetColor: Colors.white),
+            child: Checkbox(
+              value: _rememberMe,
+              checkColor: Colors.green,
+              activeColor: Colors.white,
+              onChanged: (value) {
+                setState(() {
+                  _rememberMe = value;
+                });
+              },
+            ),
+          ),
+          Text(
+            "Remember me",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLogin() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: () => print("login pressed"),
+        padding:
+            EdgeInsets.only(left: 90.0, right: 90.0, top: 12.0, bottom: 12.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        color: Hexcolor('#FFB600'),
+        child: Text(
+          "Sign in",
+          style: TextStyle(
+            color: Colors.white,
+            letterSpacing: 1.5,
+            fontSize: 24.0,
+            fontFamily: "Poppins",
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSocialMedia(Function onTap, AssetImage logo) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 60.0,
+        width: 60.0,
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          color: Colors.transparent,
+          image: DecorationImage(
+            image: logo,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSocialMediaRow() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 30.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildSocialMedia(
+            () => print("Login with Facebook"),
+            AssetImage(
+              "assets/FBButton.png",
+            ),
+          ),
+          _buildSocialMedia(
+            () => print("Login with Google"),
+            AssetImage(
+              "assets/GoogleButton.png",
+            ),
+          ),
+          _buildSocialMedia(
+            () => print("Login with Twitter"),
+            AssetImage(
+              "assets/TwitterButton.png",
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSignup() {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SignUpHome()),
+        );
+      },
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: "Don\'t have an account?",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+                fontWeight: FontWeight.w400,
+                fontFamily: "Poppins",
+              ),
+            ),
+            TextSpan(
+              text: "Sign Up",
+              style: TextStyle(
+                color: Colors.blue[400],
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: <Widget>[
+        Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/pexels-photo-172483.jpg"),
+                fit: BoxFit.cover),
+          ),
+        ),
+        Container(
+          height: double.infinity,
+          child: SingleChildScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
+            padding: EdgeInsets.symmetric(
+              horizontal: 40.0,
+              vertical: 120.0,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // Text(
+                //   "Sign In",
+                //   style: TextStyle(
+                //     color: Colors.pink[300],
+                //     fontFamily: "OpenSans",
+                //     fontSize: 30,
+                //     fontWeight: FontWeight.bold,
+                //   ),
+                // ),
+                SizedBox(height: 100.0),
+                _buildEmail(),
+                SizedBox(height: 10.0),
+                _buildPassword(),
+                _buildForgotPassword(),
+                _buildRememberMe(),
+                _buildLogin(),
+                _buildSocialMediaRow(),
+                _buildSignup(),
+              ],
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+}
---
+++ lib/signUp.dart
@@ -0,0 +1,302 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'main.dart';
+
+class SignUpHome extends StatelessWidget {
+  Widget _buildName() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: Colors.grey.withOpacity(0.5),
+            border: Border(
+              bottom: BorderSide(
+                color: Hexcolor('#FFB600'),
+                width: 3.0,
+              ),
+            ),
+          ),
+          height: 60,
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(color: Colors.white, fontFamily: "OpenSans"),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+              hintText: "Enter your full name",
+              hintStyle: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildEmail() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: Colors.grey.withOpacity(0.5),
+            border: Border(
+              bottom: BorderSide(
+                color: Hexcolor('#FFB600'),
+                width: 3.0,
+              ),
+            ),
+          ),
+          height: 60,
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(color: Colors.white, fontFamily: "OpenSans"),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.white,
+              ),
+              hintText: "Enter your email",
+              hintStyle: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPassword() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: Colors.grey.withOpacity(0.5),
+            border: Border(
+              bottom: BorderSide(
+                color: Hexcolor('#FFB600'),
+                width: 3.0,
+              ),
+            ),
+          ),
+          height: 60,
+          child: TextField(
+            obscureText: true,
+            style: TextStyle(color: Colors.white, fontFamily: "OpenSans"),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.white,
+              ),
+              hintText: "Enter your password",
+              hintStyle: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPhone() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: Colors.grey.withOpacity(0.5),
+            border: Border(
+              bottom: BorderSide(
+                color: Hexcolor('#FFB600'),
+                width: 3.0,
+              ),
+            ),
+          ),
+          height: 60,
+          child: TextField(
+            keyboardType: TextInputType.number,
+            style: TextStyle(color: Colors.white, fontFamily: "OpenSans"),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.phone,
+                color: Colors.white,
+              ),
+              hintText: "Enter your phone number",
+              hintStyle: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPostcode() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: Colors.grey.withOpacity(0.5),
+            border: Border(
+              bottom: BorderSide(
+                color: Hexcolor('#FFB600'),
+                width: 3.0,
+              ),
+            ),
+          ),
+          height: 60,
+          child: TextField(
+            style: TextStyle(color: Colors.white, fontFamily: "OpenSans"),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.white,
+              ),
+              hintText: "Enter your postcode",
+              hintStyle: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildAvatar() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: Colors.grey.withOpacity(0.5),
+            border: Border(
+              bottom: BorderSide(
+                color: Hexcolor('#FFB600'),
+                width: 3.0,
+              ),
+            ),
+          ),
+          height: 60,
+          child: TextField(
+            style: TextStyle(color: Colors.white, fontFamily: "OpenSans"),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.white,
+              ),
+              hintText: "Upload a photo (optional)",
+              hintStyle: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSignUpButton() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: () => print("login pressed"),
+        padding:
+            EdgeInsets.only(left: 90.0, right: 90.0, top: 12.0, bottom: 12.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        color: Hexcolor('#FFB600'),
+        child: Text(
+          "Let's work!",
+          style: TextStyle(
+            color: Colors.white,
+            letterSpacing: 1.5,
+            fontSize: 24.0,
+            fontFamily: "OpenSans",
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: <Widget>[
+        Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/pexels-photo-172483.jpg"),
+                fit: BoxFit.cover),
+          ),
+        ),
+        Container(
+          height: double.infinity,
+          child: SingleChildScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
+            padding: EdgeInsets.symmetric(
+              horizontal: 40.0,
+              vertical: 120.0,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 10.0),
+                _buildName(),
+                SizedBox(height: 10.0),
+                _buildEmail(),
+                SizedBox(height: 10.0),
+                _buildPhone(),
+                SizedBox(height: 10.0),
+                _buildPassword(),
+                SizedBox(height: 10.0),
+                _buildPostcode(),
+                SizedBox(height: 10.0),
+                _buildAvatar(),
+                _buildSignUpButton(),
+                // _buildLogin(),
+              ],
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+}
