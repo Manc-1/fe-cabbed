4047fa07663e5a8c5d6caa82e324a4705d25abab
cocoflade <lukeflannery1992@hotmail.com>
2020-05-27 09:43:49 +0100

    installed progress bar dependencies

--- lib/logInPage.dart
+++ lib/logInPage.dart
@@ -10,6 +10,8 @@ import 'mapPage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_twitter_login/flutter_twitter_login.dart';
 
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
--- lib/socialLogin.dart
+++ lib/socialLogin.dart
@@ -1,97 +1,102 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_facebook_login/flutter_facebook_login.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
-// //import 'package:flutter_twitter/flutter_twitter.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter_twitter_login/flutter_twitter_login.dart';
-// import 'package:http/http.dart' as http;
-// import 'dart:convert' as JSON;
-// import 'mapPage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+//import 'package:flutter_twitter/flutter_twitter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_twitter_login/flutter_twitter_login.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as JSON;
+import 'mapPage.dart';
+import 'dart:async';
+import 'dart:convert';
 
 // void main() => runApp(MyApp());
 
-// // class MyApp extends StatefulWidget {
-// //   @override
-// //   State<StatefulWidget> createState() {
-// //     // TODO: implement createState
-// //     return _MyAppState();
-// //   }
-// // }
+class SocialLogin extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _SocialLoginState();
+  }
+}
 
-// // class _MyAppState extends State<MyApp> {
-// //   bool _isLoggedIn = false;
-// //   Map userProfile;
-// //   final facebookLogin = FacebookLogin();
-// //   final googleSignin = GoogleSignIn();
+class _SocialLoginState extends State<SocialLogin> {
+  @override
+  _loginWithFB() async {
+    bool _isLoggedIn = false;
+    Map userProfile;
+    final facebookLogin = FacebookLogin();
+    final googleSignin = GoogleSignIn();
 
-// //   _loginWithFB() async {
-// //     final result = await facebookLogin.logInWithReadPermissions(['email']);
+    final result = await facebookLogin.logInWithReadPermissions(['email']);
 
-// //     switch (result.status) {
-// //       case FacebookLoginStatus.loggedIn:
-// //         final token = result.accessToken.token;
-// //         final graphResponse = await http.get(
-// //             'https://graph.facebook.com/v2.12/me?fields=name,picture,email&access_token=${token}');
-// //         final profile = JSON.jsonDecode(graphResponse.body);
-// //         print(profile);
-// //         setState(() {
-// //           userProfile = profile;
-// //           _isLoggedIn = true;
-// //         });
-// //         break;
+    switch (result.status) {
+      case FacebookLoginStatus.loggedIn:
+        final token = result.accessToken.token;
+        final graphResponse = await http.get(
+            'https://graph.facebook.com/v2.12/me?fields=name,picture,email&access_token=${token}');
+        final profile = JSON.jsonDecode(graphResponse.body);
+        print(profile);
+        setState(() {
+          userProfile = profile;
+          _isLoggedIn = true;
+        });
+        break;
 
-// //       case FacebookLoginStatus.cancelledByUser:
-// //         setState(() => _isLoggedIn = false);
-// //         break;
-// //       case FacebookLoginStatus.error:
-// //         setState(() => _isLoggedIn = false);
-// //         break;
-// //     }
-// //   }
+      case FacebookLoginStatus.cancelledByUser:
+        setState(() => _isLoggedIn = false);
+        break;
+      case FacebookLoginStatus.error:
+        setState(() => _isLoggedIn = false);
+        break;
+    }
+  }
 
-// //   _logout() {
-// //     facebookLogin.logOut();
-// //     setState(() {
-// //       _isLoggedIn = false;
-// //     });
-// //   }
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
+// _logout() {
+//   facebookLogin.logOut();
+//   setState(() {
+//     _isLoggedIn = false;
+//   });
+// }
 
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     // TODO: implement build
-// //     return MaterialApp(
-// //       home: Scaffold(
-// //         body: Center(
-// //             child: _isLoggedIn
-// //                 ? Column(
-// //                     mainAxisAlignment: MainAxisAlignment.center,
-// //                     children: <Widget>[
-// //                       Image.network(
-// //                         userProfile["picture"]["data"]["url"],
-// //                         height: 50.0,
-// //                         width: 50.0,
-// //                       ),
-// //                       Text(userProfile["name"]),
-// //                       OutlineButton(
-// //                         child: Text("Logout"),
-// //                         onPressed: () {
-// //                           _logout();
-// //                         },
-// //                       )
-// //                     ],
-// //                   )
-// //                 : Center(
-// //                     child: OutlineButton(
-// //                       child: Text("Login with Facebook"),
-// //                       onPressed: () {
-// //                         _loginWithFB();
-// //                       },
-// //                     ),
-// //                   )),
-// //       ),
-// //     );
-// //   }
-// // }
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//             child: _isLoggedIn
+//                 ? Column(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     children: <Widget>[
+//                       Image.network(
+//                         userProfile["picture"]["data"]["url"],
+//                         height: 50.0,
+//                         width: 50.0,
+//                       ),
+//                       Text(userProfile["name"]),
+//                       OutlineButton(
+//                         child: Text("Logout"),
+//                         onPressed: () {
+//                           _logout();
+//                         },
+//                       )
+//                     ],
+//                   )
+//                 : Center(
+//                     child: OutlineButton(
+//                       child: Text("Login with Facebook"),
+//                       onPressed: () {
+//                         _loginWithFB();
+//                       },
+//                     ),
+//                   )),
+//       ),
+//     );
+//   }
+// }
 
 // // Twitter login authentication
 // class TwLogin extends StatelessWidget {
