c4e2b3f1798c4c6f206eb42822795908b0c23b88
cocoflade <lukeflannery1992@hotmail.com>
2020-05-28 22:04:44 +0100

    popup menu functionality moved to app bar various styling

--- lib/logInPage.dart
+++ lib/logInPage.dart
@@ -12,7 +12,6 @@ import 'mapPage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_twitter_login/flutter_twitter_login.dart';
 
-
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'dart:convert' as JSON;
 
@@ -28,13 +27,11 @@ class _HomeState extends State<Home> {
     Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
   }
 
-  Future navigateToLoginPage(context,Object userdata) async {
+  Future navigateToLoginPage(context, Object userdata) async {
     debugPrint(userdata);
     Navigator.push(
         context, MaterialPageRoute(builder: (context) => MapSample()));
   }
-
-
 
   final userEmail = TextEditingController();
   final userPassword = TextEditingController();
@@ -56,9 +53,8 @@ class _HomeState extends State<Home> {
     setState(() {
       var resBody = json.decode(response.body);
       data = resBody["msg"];
-      
     });
-    
+
     debugPrint(password);
     debugPrint(email);
     debugPrint(data);
